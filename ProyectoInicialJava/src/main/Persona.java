b4de9be412bc11e7df190bfcb5f6e87ab3f286e1
Mikelak <higginspi@hotmail.com>
2015-05-29 13:44:32 +0200

    Serialización

--- ProyectoInicialJava/src/main/Persona.java
+++ ProyectoInicialJava/src/main/Persona.java
@@ -1,6 +1,8 @@
 package main;
 
-public class Persona {
+import java.io.Serializable;
+
+public class Persona implements Serializable {
 
 	private String nombre;
 	private int edad;
---
+++ ProyectoInicialJava/src/main/Serialización.java
@@ -0,0 +1,58 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.function.ObjLongConsumer;
+
+public class Serialización {
+
+	public static void main(String[] args) 
+	{
+		serializaPersonas();
+		// Obtenemos las Personas serializadas y comprobamos su estado
+		try 
+		{
+			ObjectInputStream obj_entrada = new ObjectInputStream(new FileInputStream("objetos.dat"));
+			System.out.println(((Persona)obj_entrada.readObject()).getNombre());
+			System.out.println(((Persona)obj_entrada.readObject()).getNombre());
+			System.out.println(((Persona)obj_entrada.readObject()).getNombre());
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void serializaPersonas()
+	{
+		ArrayList<Persona> lista_personas = new ArrayList<Persona>();
+		// Persona debe implementar la interfaz Serializable
+		Persona p1 = new Persona("Pepe", 21);
+		Persona p2 = new Persona("Juan", 23);
+		Persona p3 = new Persona("Ildefonso", 99);		
+		lista_personas.add(p1);
+		lista_personas.add(p2);
+		lista_personas.add(p3);
+		
+		try 
+		{
+			ObjectOutputStream obj_salida = new ObjectOutputStream(new FileOutputStream("objetos.dat"));
+			for (Persona p : lista_personas)
+			{
+				obj_salida.writeObject(p);
+			}
+			obj_salida.close();
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+}
